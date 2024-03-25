6d736bf02280a704e76cae625996f2ded8abf3d5
Inga <inga@snowschool.pl>
2024-03-25 12:08:38 +0100

    url fixes

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,12 @@
-import { language } from "fontawesome";
+import { useEffect, useState } from "react";
 
-export const Navigation = (props) => {
+export function Navigation(props) {
+  let [about, setAbout] = useState(false);
+  let [offer, setOffer] = useState(false);
+  let [fleet, setFleet] = useState(false);
+  let [gallery, setGallery] = useState(false);
+  let [contact, setContact] = useState(false);
+
   let content = {
     English: {
       block1: "About Us",
@@ -11,89 +17,121 @@ export const Navigation = (props) => {
       block6: "About_Us",
     },
     Polski: {
-        block1: "O nas",
-        block2: "Flota",
-        block3: "Oferta",
-        block4: "Galeria",
-        block5: "Kontakt",
-        block6: "O_Nas",
-      }
-    }
+      block1: "O nas",
+      block2: "Flota",
+      block3: "Oferta",
+      block4: "Galeria",
+      block5: "Kontakt",
+      block6: "O_Nas",
+    },
+  };
 
+  function remove_hash_from_url() {
+    var uri = window.location.toString();
+
+    if (uri.indexOf("#") > 0) {
+      var clean_uri = uri.replace("#", "");
+      window.history.replaceState({}, document.title, clean_uri);
+    }
+  }
+
+  var category = "";
+
+  function clearUrl() {
+    var uri = window.location.toString();
+    var n = uri.lastIndexOf("/");
+
+    category = uri.substring(n + 1, uri.length);
+
+    if (category.length >= 0) {
+      var cleared_url = uri.substring(0, n);
+      window.history.replaceState({}, document.title, cleared_url);
+      console.log(cleared_url);
+    }
+  }
 
   props.language === "Polski"
-  ? (content = content.Polski)
-  : (content = content.English);
+    ? (content = content.Polski)
+    : (content = content.English);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      remove_hash_from_url();
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
-      <div className='container'>
-        <div className='navbar-header'>
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
           <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
           >
-            {' '}
-
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
+            {" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
           </button>
-          <a className='navbar-brand navbar-logo page-scroll' href='#page-top'>
-          <img src="img/sea2.png" className="img-responsive nav-logo" alt="logo" />{" "}
-          </a>{' '}
-        
+          <a className="navbar-brand navbar-logo page-scroll" href="#page-top">
+            <img
+              src="img/sea2.png"
+              className="img-responsive nav-logo"
+              alt="logo"
+            />{" "}
+          </a>{" "}
         </div>
         <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
         >
-          <ul className='nav navbar-nav navbar-right'>
+          <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#aboutus" className='page-scroll'>
-            {content.block1}
+              <a
+                href="#aboutus"
+                className="page-scroll"
+                onClickCapture={clearUrl}
+              >
+                {content.block1}
               </a>
             </li>
             <li>
-              <a href="#offer" className='page-scroll'>
-              {content.block3}
+              <a href="#offer" className="page-scroll" onClick={clearUrl}>
+                {content.block3}
               </a>
             </li>
             <li>
-              <a href="#fleet" className='page-scroll'>
-              {content.block2}
+              <a href="#fleet" className="page-scroll" onClick={clearUrl}>
+                {content.block2}
               </a>
             </li>
             <li>
-              <a href="#gallery" className='page-scroll'>
-              {content.block4}
+              <a href="#gallery" className="page-scroll" onClick={clearUrl}>
+                {content.block4}
               </a>
             </li>
-          
+
             <li>
-              <a href="#contact" className='page-scroll'>
-              {content.block5}
+              <a href="#contact" className="page-scroll" onClick={clearUrl}>
+                {content.block5}
               </a>
             </li>
           </ul>
         </div>
-       
-       
       </div>
       <div className="selector">
-          <select
-            className="custom-select"
-            value={props.language}
-            onChange={e => props.handleSetLanguage(e.target.value)}
-          >
-            <option value="English">English</option>
-            <option value="Polski">Polski</option>
-          </select>
-         
-          </div>
+        <select
+          className="custom-select"
+          value={props.language}
+          onChange={(e) => props.handleSetLanguage(e.target.value)}
+        >
+          <option value="English">English</option>
+          <option value="Polski">Polski</option>
+        </select>
+      </div>
     </nav>
-  )
+  );
 }
