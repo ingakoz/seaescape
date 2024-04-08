3097dba440353c7932e60faf39fc4fc43604f89d
Inga <inga@snowschool.pl>
2024-04-08 17:23:46 +0200

    url updates

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -20,40 +20,40 @@ export function Navigation(props) {
     },
   };
 
-  function remove_hash_from_url() {
-    var uri = window.location.toString();
+  // function remove_hash_from_url() {
+  //   var uri = window.location.toString();
 
-    if (uri.indexOf("#") > 0) {
-      var clean_uri = uri.replace("#", "");
-      window.history.replaceState({}, document.title, clean_uri);
-    }
-  }
+  //   if (uri.indexOf("#") > 0) {
+  //     var clean_uri = uri.replace("#", "");
+  //     window.history.replaceState({}, document.title, clean_uri);
+  //   }
+  // }
 
   var category = "";
 
-  function clearUrl() {
-    var uri = window.location.toString();
-    var n = uri.lastIndexOf("/");
+  // function clearUrl() {
+  //   var uri = window.location.toString();
+  //   var n = uri.lastIndexOf("/");
 
-    category = uri.substring(n + 1, uri.length);
+  //   category = uri.substring(n + 1, uri.length);
 
-    if (category.length >= 0) {
-      var cleared_url = uri.substring(0, n);
-      console.log(cleared_url);
-      window.history.replaceState({}, document.title, cleared_url);
-    }
-  }
+  //   if (category.length >= 0) {
+  //     var cleared_url = uri.substring(0, n);
+  //     console.log(cleared_url);
+  //     window.history.replaceState({}, document.title, cleared_url);
+  //   }
+  // }
 
   props.language === "Polski"
     ? (content = content.Polski)
     : (content = content.English);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      remove_hash_from_url();
-    }, 500);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     remove_hash_from_url();
+  //   }, 500);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -75,7 +75,6 @@ export function Navigation(props) {
               src="img/sea2.png"
               className="img-responsive nav-logo"
               alt="logo"
-              onClick={clearUrl}
             />{" "}
           </a>{" "}
         </div>
@@ -85,28 +84,28 @@ export function Navigation(props) {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#aboutus" className="page-scroll" onClick={clearUrl}>
+              <a href="#aboutus" className="page-scroll" >
                 {content.block1}
               </a>
             </li>
             <li>
-              <a href="#offer" className="page-scroll" onClick={clearUrl}>
+              <a href="#offer" className="page-scroll" >
                 {content.block3}
               </a>
             </li>
             <li>
-              <a href="#fleet" className="page-scroll" onClick={clearUrl}>
+              <a href="#fleet" className="page-scroll" >
                 {content.block2}
               </a>
             </li>
             <li>
-              <a href="#gallery" className="page-scroll" onClick={clearUrl}>
+              <a href="#gallery" className="page-scroll">
                 {content.block4}
               </a>
             </li>
 
             <li>
-              <a href="#contact" className="page-scroll" onClick={clearUrl}>
+              <a href="#contact" className="page-scroll" >
                 {content.block5}
               </a>
             </li>
